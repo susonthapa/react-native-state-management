f3f5e8ccba83cb89b29a16c3def0a4357aeed53a
susonthapa <33973551+susonthapa@users.noreply.github.com>
2024-04-17 16:29:14 +0545

    added jotai

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,51 @@
 import { Button, StyleSheet } from 'react-native';
 
 import { Text, View } from '@/components/Themed';
-import { useState } from 'react';
+import { Provider, atom, useAtom, useAtomValue } from 'jotai';
+
+const countAtom = atom(0)
+const multiplierAtom = atom((get) => {
+  const count = get(countAtom)
+  return count * 10
+})
+
+const Counter = () => {
+  const [count, setCount] = useAtom(countAtom)
+  const multiplierCount = useAtomValue(multiplierAtom)
+  return (
+      <View>
+        <Text>Hello World!</Text>
+        <View style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+          <Button title='Decrement' onPress={() => {
+            setCount((count) => count - 1)
+          }} />
+          <Text>The Count is {count}</Text>
+          <Button title='Increment' onPress={() => {
+            setCount((count) => count + 1)
+          }} />
+        </View>
+        <Text>Multiplied Count: {multiplierCount}</Text>
+      </View>
+  )
+}
+
+const CounterWithProvider = () => {
+  return (
+    <Provider>
+      <Counter />
+    </Provider>
+  )
+}
 
 export default function TabOneScreen() {
-  const [count, setCount] = useState(0)
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <View style={{
-        flexDirection: 'row',
-        alignItems: 'center',
-      }}>
-        <Button title='Decrement' onPress={() => {
-          setCount((count) => count - 1)
-        }} />
-        <Text>The Count is {count}</Text>
-        <Button title='Increment' onPress={() => {
-          setCount((count) => count + 1)
-        }} />
-      </View>
+      <CounterWithProvider />
+      <CounterWithProvider />
+      <CounterWithProvider />
     </View>
   );
 }
