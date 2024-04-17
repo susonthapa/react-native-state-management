b87e2a8bc69c64ac27221f579ad442975ba3c33c
susonthapa <33973551+susonthapa@users.noreply.github.com>
2024-04-17 20:42:16 +0545

    added zustand

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,65 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { createContext, useContext, useRef } from 'react';
+import { Button, StyleSheet } from 'react-native';
+import { create, useStore } from 'zustand';
+
+interface CounterState {
+  count: number,
+  increment: () => void,
+  decrement: () => void,
+}
+
+const createCounterStore = () => create<CounterState>((set) => ({
+  count: 0,
+  increment: () => set((state) => ({ count: state.count + 1 })),
+  decrement: () => set((state) => ({ count: state.count - 1 }))
+}))
+
+type CounterStore = ReturnType<typeof createCounterStore>
+const CounterContext = createContext<CounterStore | null>(null)
+
+function useCounterContext<T>(selector: (state: CounterState) => T): T {
+  const store = useContext(CounterContext)
+  if (!store) throw new Error('Missing CounterContext.Provider in the tree')
+  return useStore(store, selector)
+}
+
+const Counter = () => {
+  console.log(`Counter Rendering`);
+  const count = useCounterContext(state => state.count)
+  const increment = useCounterContext(state => state.increment)
+  const decrement = useCounterContext(state => state.decrement)
+  return (
+    <View style={{
+      flexDirection: 'row',
+      alignItems: 'center'
+    }}>
+      <Button title='Decrement' onPress={decrement} />
+      <Text>Counter is {count}</Text>
+      <Button title='Increment' onPress={increment} />
+    </View>
+  )
+}
+
+const CounterWithProvider = () => {
+  const storeRef = useRef<CounterStore>()
+  if (!storeRef.current) {
+    storeRef.current = createCounterStore()
+  }
+  return (
+    <CounterContext.Provider value={storeRef.current}>
+      <Counter />
+    </CounterContext.Provider>
+  )
+}
 
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <CounterWithProvider />
+      <CounterWithProvider />
+      <CounterWithProvider />
+      <CounterWithProvider />
     </View>
   );
 }
